97ff20d2d4b866858979422499dbca4b22f215c7
Semyon <gsemyong@gmail.com>
2022-05-08 00:01:03 +0300

    Working App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,140 @@
-import Link from "next/link";
 import {
+  ActionIcon,
   Button,
+  Checkbox,
   Container,
   Group,
-  List,
-  Text,
+  Space,
+  Stack,
   TextInput,
   Title,
 } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "@mantine/hooks";
+import { IconTrash } from "@tabler/icons";
 
 function Home() {
-  const [todoItems, setTodoItems] = useState<Array<string>>([]);
+  const [todoItems, setTodoItems] = useState<Array<todoItem>>([]);
+
   const form = useForm({
     initialValues: {
       todoText: "",
     },
     validationRules: {
-      todoText: (value) =>
-        value !== "" && todoItems.find((item) => item === value) == undefined,
+      todoText: (value) => value !== "",
     },
   });
 
+  useEffect(() => {
+    const todoItemStr = localStorage.getItem("todoItems");
+
+    if (todoItemStr !== null) {
+      setTodoItems(JSON.parse(todoItemStr));
+    }
+  }, []);
+
   return (
-    <Container size="xs">
+    <Container py={40} size="xs">
       <Title order={1} align="center">
         Todo App
       </Title>
 
+      <Space h="xl" />
+
       <form
-        onSubmit={form.onSubmit((values) =>
-          setTodoItems([...todoItems, values.todoText])
-        )}
+        onSubmit={form.onSubmit((values) => {
+          form.setFieldValue("todoText", "");
+          setTodoItems((items) => {
+            const updatedItems = [
+              ...items,
+              {
+                id: Date.now(),
+                todoText: values.todoText,
+                completed: false,
+              },
+            ];
+
+            localStorage.setItem("todoItems", JSON.stringify(updatedItems));
+
+            return updatedItems;
+          });
+        })}
       >
         <Group position="center" align="flex-end">
           <TextInput
             placeholder="Enter your Todo"
-            label="Todo Input"
             {...form.getInputProps("todoText")}
           />
           <Button type="submit">Add Todo</Button>
         </Group>
       </form>
-      <List>
-        {todoItems.map((item) => (
-          <List.Item key={item}>{item}</List.Item>
-        ))}
-      </List>
+      <Space h="xl" />
+      <Container size="xs">
+        <Stack>
+          {todoItems.map((item) => (
+            <Group key={item.id} align="center" position="apart">
+              <Checkbox
+                checked={item.completed}
+                onChange={(event) => {
+                  setTodoItems((items) => {
+                    const updatedItems = items.map((i) => {
+                      if (i.id == item.id) {
+                        return {
+                          ...i,
+                          completed: !i.completed,
+                        };
+                      } else {
+                        return i;
+                      }
+                    });
+
+                    localStorage.setItem(
+                      "todoItems",
+                      JSON.stringify(updatedItems)
+                    );
+
+                    return updatedItems;
+                  });
+                }}
+                size="md"
+                label={item.todoText}
+                styles={{
+                  label: {
+                    color: item.completed ? "GrayText" : "white",
+                    textDecoration: item.completed ? "line-through" : "initial",
+                  },
+                }}
+              />
+              <ActionIcon variant="filled" color="red">
+                <IconTrash
+                  onClick={() => {
+                    setTodoItems((items) => {
+                      const updatedItems = items.filter(
+                        (filteredItem) => filteredItem.id != item.id
+                      );
+
+                      localStorage.setItem(
+                        "todoItems",
+                        JSON.stringify(updatedItems)
+                      );
+
+                      return updatedItems;
+                    });
+                  }}
+                />
+              </ActionIcon>
+            </Group>
+          ))}
+        </Stack>
+      </Container>
     </Container>
   );
+}
+
+interface todoItem {
+  id: number;
+  todoText: string;
+  completed: boolean;
 }
 
 export default Home;
